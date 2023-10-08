74780b8c467c1a378a60f0bd1bd54264fe2bac3e
pistanik <piotr.stnk@gmail.com>
2023-10-08 16:09:08 +0200

    Dodano funkcję liczącą obwód kwadratu

--- src/Square.java
+++ src/Square.java
@@ -1,10 +1,17 @@
 public class Square {
     public static void main(String[] args) {
         pole(5);
+        obwod(5);
     }
 
     public static void   pole(int bok){
         int pole = bok * bok;
         System.out.println("Pole kwadratu o boku " + bok + " wynosi: " + pole);
     }
+
+    public static void obwod(int bok){
+        int obwod = 4 * bok;
+        System.out.println("Obwód kwadratu o boku " + bok + " wynosi: " + obwod);
+    }
+
 }
