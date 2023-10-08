0fddac5363cd6fa34bd8860d0b577f198dad309b
pistanik <piotr.stnk@gmail.com>
2023-10-08 16:01:43 +0200

    Initial commit

---
+++ src/Square.java
@@ -0,0 +1,10 @@
+public class Square {
+    public static void main(String[] args) {
+        pole(5);
+    }
+
+    public static void   pole(int bok){
+        int pole = bok * bok;
+        System.out.println("Pole kwadratu o boku " + bok + " wynosi: " + pole);
+    }
+}
