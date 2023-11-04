6038aa72958380aa5c0f675dd2aeb36034f6867c
pistanik <piotr.stnk@gmail.com>
2023-11-04 10:31:18 +0100

    h

---
+++ src/Kolokwium/zjazd2.java
@@ -0,0 +1,16 @@
+package Kolokwium;
+
+public class zjazd2 {
+//
+//    Napisz program przechowujący w odzielnych zmiennych Twoje imie, nazwisko oraz wiek. Użyj przygotowanych zmiennych do wyświetlenia na ekranie informacji wg poniższego wzoru.
+//
+//    Nazywam się ............ i mam ......... lat.
+
+    public static void main(String[] args) {
+        String name = "Piotr";
+        String surname = "Stanik";
+        int age = 30;
+
+        System.out.println("Nazywam się " + name + " " + surname + " i mam " + age + " lat.");
+    }
+}
