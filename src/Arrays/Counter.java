6fb1c12e912793eea80fbbfd35e4b2467eb56770
pistanik <piotr.stnk@gmail.com>
2023-11-05 12:54:08 +0100

    Drugi zjazd - niedziela

---
+++ src/Arrays/Counter.java
@@ -0,0 +1,65 @@
+package Arrays;
+
+public class Counter {
+    public static void main(String[] args) {
+
+//        String[] cyfry = {"4", "1", "1", "4", "2", "3", "4", "4", "1", "2", "4", "9", "3"};
+//
+//        int counter1 = 0;
+//        int counter2 = 0;
+//        int counter3 = 0;
+//        int counter4 = 0;
+//        int counter9 = 0;
+//
+//        for (int i = 0; i < cyfry.length; i++) {
+//
+//            switch (cyfry[i]){
+//
+//                case "1":
+//                    counter1 +=1;
+//                    break;
+//                case "2":
+//                    counter2 +=1;
+//                    break;
+//                case "3":
+//                    counter3 += 1;
+//                    break;
+//                case "4":
+//                    counter4 += 1;
+//                    break;
+//                case "9":
+//                    counter9 +=1;
+//                    break;
+//
+//            }
+//        }
+//
+////        Math.max(counter3,(Math.max(counter1, counter2));
+//        System.out.println(counter1);
+//        System.out.println(counter2);
+//        System.out.println(counter3);
+//        System.out.println(counter4);
+//        System.out.println(counter9);
+
+//        Rozwiązanie miklas:
+
+        int[] arr = {3, 6, 3, 5, 5, 2, 6, 8, 0, 1,4,2,1,5,521,34,1243,521,521,521}; // jeśli chcemy zwiekszyc zakres, tzn nie tylko cyfry ale tez liczby to treba
+                                                                                    // zmienic zakres drugiego ciagu
+
+
+        int[] counters = new int[1000000];
+
+        for (int i = 0; i < arr.length; i++) {
+            counters[arr[i]]++; // zwiększa odpowiedni wyraz tablicy jesli ona występuje
+        }
+
+        int mostFrequent = 0;
+            for (int i = 0; i < counters.length; i++) {
+            if(counters[i] > counters[mostFrequent]){ // przesuwa wskazanie na najczesciej wystepujący wyraz
+                mostFrequent = i;
+            }
+        }
+
+        System.out.println("Najczęściej występującą cyfrą w tablicy jest cyfra: " + mostFrequent+ ", wystąpiła " + counters[mostFrequent] + " razy");
+    }
+}
---
+++ src/Arrays/ThreeDimensionalArray.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class ThreeDimensionalArray {
+    public static void main(String[] args) {
+
+//        int[] arr1 = {1, 2, 5, 123, 54, 65, 64};
+//        System.out.println(arr1.length);
+//
+//        int[] arr2 = {9, 14, 2, 23, 154, 12, 0};
+//        System.out.println(arr1.length);
+//
+//        int[] arr3 = {0, 6, 8, 541, 54, 65, 23};
+//        System.out.println(arr1.length);
+//
+////        int[][] multiArray = {arr1, arr2 , arr3};
+
+
+        int [][][]  multi = {{{1, 3,5,3}},{{71,124},{2,415,65}}};
+
+        for (int i = 0; i < multi.length; i++) {
+            for (int j  = 0; j <multi[i].length ; j++) {
+                for (int k = 0; k < multi[i][j].length; k++) {
+                    System.out.print("arr[" + i + "]["+ j + "]["+ k + "] = ");
+                    System.out.println(multi[i][j][k]);
+                }
+            }
+        }
+    }
+}
