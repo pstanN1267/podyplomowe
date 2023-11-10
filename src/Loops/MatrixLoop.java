f3c0920170a3cfa24b19efc18a1010780f169cae
pstanN1267 <piotr.stnk@gmail.com>
2023-11-10 21:39:26 +0100

    Lab7

--- src/Loops/MatrixLoop.java
+++ src/Loops/MatrixLoop.java
@@ -3,16 +3,22 @@ package Loops;
 public class MatrixLoop {
     public static void main(String[] args) {
 
-        int n = 10;
+        int n = 1;
         System.out.println("n = " + n + "   matrix");
         System.out.println("-----------------");
         for (int i = 1; i < n + 1; i++) { // najpierw zewnętrzna pętla się wykonuje pełną ilość razy, następnie wewnetrzna
-            System.out.print(" " + i + "  ");
-
+            System.out.printf ("%3s",i + " ");
             for (int j = 1; j < n ; j++) {
-                System.out.printf ("%4s", j + i + " ");
+                System.out.printf ("%3s", j + i + " ");
             }
             System.out.println();
         }
+
+//        for (int i = 0; i < n; i++) { // najpierw zewnętrzna pętla się wykonuje pełną ilość razy, następnie wewnetrzna
+//            for (int j = 1; j <= n ; j++) {
+//                System.out.printf ("%3s", j + i + " ");
+//            }
+//            System.out.println();
+//        }
     }
 }
