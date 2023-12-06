e934e87bdcf6930299506965e0e7a6d2b3a77d55
pstanN1267 <piotr.stnk@gmail.com>
2023-12-06 20:44:41 +0100

    Ukończone domowe 1

--- src/domoweZjazd2/zad1.java
+++ src/domoweZjazd2/zad1.java
@@ -4,38 +4,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class zad1 {
-    //    Napisz program, który:
-//    stworzy tablicę liczb typu int o rozmiarze, który będziemy podawać jako argument przy starcie programu,
-//    wypełni tablicę kolejno liczbami od 1 do n, gdzie n to rozmiar tablicy,
-//    wyświetli zawartość tablicy,
-//    dokona konwersji tablicy tak, aby elementy ułożyły się odwrotnie (ciąg malejący: n, n-1, n-2… 1),
-//    wyświetli zawartyość przekonwertowanej tablicy,
-//    fragmenty programu odpowiadjące za konkretne zadania zostaną wyodrębnione jako oddzielne metody.
 
-//    ad 1 metoda zwracająca tablicę z argumentem ilosci wyrazów tablicy
 
-    public static int[] arrayMaker (int n){
+    public static int[] arrayMaker(int n) {
         int[] array = new int[n];
         for (int i = 0; i < n; i++) {
             array[i] = i;
         }
-        System.out.println(Arrays.toString(array));
 
+        System.out.println("Array: " + Arrays.toString(array));
         return array;
     }
 
-    public static void reverseArray(int n) {
-      int [] arrayToReverse = arrayMaker(n);
-        int [] reverseArray =new int[arrayToReverse.length];
+    public static void reverseArray(int[] array, int n) {
+        int[] reversedArray = new int[array.length];
 
-        for (int i = 0; i < reverseArray.length; i++) {
-            reverseArray[arrayMaker(n).length - i] = arrayMaker(n)[i];
-//            System.out.println((arrayMaker(n).length - i) + " = " + arrayMaker(n)[i] );
+        for (int i = 0; i < array.length; i++) {
+            reversedArray[i] = array[n - i - 1];
         }
-        System.out.println(Arrays.toString(reverseArray));
-//        return reverseArray;
-    }
 
+        System.out.println("Reversed array: " + Arrays.toString(reversedArray));
+
+    }
 
 
     public static void main(String[] args) {
@@ -44,6 +34,7 @@ public class zad1 {
         System.out.println("Podaj długość tablicy: ");
         int n = scanner.nextInt();
         arrayMaker(n);
-//        reverseArray(n);
+
+        reverseArray(arrayMaker(n), n);
     }
 }
