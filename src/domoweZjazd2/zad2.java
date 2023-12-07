16e7f9e520d09343aa692c93ee34a002a0e240d4
pstanN1267 <piotr.stnk@gmail.com>
2023-12-07 22:01:35 +0100

    poprawa lab7.1

--- src/domoweZjazd2/zad2.java
+++ src/domoweZjazd2/zad2.java
@@ -20,16 +20,41 @@ public class zad2 {
 
 
 
-        for (int i = 0; i < wiersze; i++) {
+        for (int i = 1; i < wiersze; i++) {
             for (int j = wiersze - i; j > 1; j--) {
                 System.out.print(" ");
             }
 
-            for (int j = 0; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 int  index = new Random().nextInt(choinka.length);
                 System.out.print(choinka[index] + " ");
             }
             System.out.println();
         }
+
+//
+//        int row = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        if (row < 1) {
+//            row = 18;
+//        }
+//        char[] choinka = {'+', '.', '*', '~', '^', 'o'};
+//
+//
+//
+//        int space = row - 1;
+//        for (int j = 1; j <= row; j++) {
+//            for (int i = 1; i <= space; i++) {
+//                System.out.print(" ");
+//            }
+//            space--;
+//            for (int i = 1; i <= 2 * j - 1; i++) {
+//                int index = new Random().nextInt(choinka.length);
+//                System.out.print(choinka[index]);
+//            }
+//            System.out.println();
+//        }
     }
 }
+
